d9c55bfa2a67def066b23af132da80f68f7f66ec
Vladimir Sadovnikov <v.sadovnikov@isp.local>
2019-03-11 15:22:27 +0300

    add gitignore

--- dev/db_connect.php
+++ dev/db_connect.php
@@ -1,8 +1,6 @@
 <?php
+include_once 'config.php';
 include_once 'classes/safemysql.class.php';
-$dbuser =   'sk8kilay_test';
-$dbpass =   'hq2*9gW1';
-$dbname =   'sk8kilay_test';
-$host   =   'localhost';
+
 $db = new SafeMySQL(array("host" => "$host", "user" => "$dbuser", "pass" => "$dbpass", "db" => "$dbname", "charset" => "utf8"));
 ?>
